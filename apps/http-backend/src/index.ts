b835536c371e29ce29abc3276337ee5f1192535b
Yash Kalange <yashkalange127@gmail.com>
2025-08-21 23:44:49 +0530

    added signin signup and create authMiddleware

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,20 +1,28 @@
 import express from "express"
+import jwt from "jsonwebtoken"
+import { JWT_SECRET } from "./config";
+import { authMiddleware } from "./middleware";
 
 const app = express();
 
 app.get('/signup' ,(req,res)=>{
     const body = req.body;
 
-    
+
 
     res.send(400)
 })
 app.get('/signin' ,(req,res)=>{
     const body = req.body;
 
-    res.send(400)
+    const userId = 1;
+    const token = jwt.sign({userId},JWT_SECRET);
+
+    res.send({
+        token
+    })
 })
-app.get('/create-room' ,(req,res)=>{
+app.get('/room',authMiddleware ,(req,res)=>{
     const body = req.body;
 
     res.send(400)
---
+++ apps/http-backend/src/middleware.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+
+export function authMiddleware(req: Request, res: Response, next: NextFunction) {
+    const token = req.header("authorization");
+
+    if (!token) {
+        return res.status(401).json({ message: "No token provided" });
+    }
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET) as { userId: string };
+        req.userId = decoded.userId; 
+        next();
+    } catch {
+        res.status(403).json({ message: "Invalid token" });
+    }
+}
