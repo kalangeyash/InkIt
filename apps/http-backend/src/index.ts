729d91d73a9e050b37f2479cc7346fb38b98c9be
Yash Kalange <yashkalange127@gmail.com>
2025-08-31 11:44:27 +0530

    created zod common for fe and be

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,17 +2,32 @@ import express from "express"
 import jwt from "jsonwebtoken"
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { authMiddleware } from "./middleware";
+import { CreateUserSchema , SignInSchema , CreateRoomSchema } from "@repo/common/types.ts";
 
 const app = express();
 
 app.get('/signup' ,(req,res)=>{
+    const data = CreateUserSchema.safeParse(req.body);
+    if(!data.success)
+    { 
+        res.json({
+            message:"Incorreect Inputs"
+        })
+        return;
+    }
     res.send(200).json({
         userId:1234
     })
 })
 app.get('/signin' ,(req,res)=>{
-    const body = req.body;
-
+    const data = SignInSchema.safeParse(req.body);
+    if(!data.success)
+    { 
+        res.json({
+            message:"Incorreect Inputs"
+        })
+        return;
+    }
     const userId = 1;
     const token = jwt.sign({userId},JWT_SECRET);
 
@@ -21,7 +36,14 @@ app.get('/signin' ,(req,res)=>{
     })
 })
 app.get('/room',authMiddleware ,(req,res)=>{
-    
+    const data = CreateRoomSchema.safeParse(req.body);
+    if(!data.success)
+    { 
+        res.json({
+            message:"Incorreect Inputs"
+        })
+        return;
+    }
 
     res.send(200).json({
         roomId:123
